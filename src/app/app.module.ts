1f71246232ef6dda76eca6665946982e2d83344a
Asplap3275 <kuralanban.karthi@aspiresys.com>
2022-12-20 20:22:43 +0530

    added Global styles to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { CheckAgePipe } from './pipes/check-age.pipe';
+import { StructuralDirectivesComponent } from './structural-directives/structural-directives.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatabindingComponent,
+    CheckAgePipe,
+    StructuralDirectivesComponent
   ],
   imports: [
     BrowserModule,
